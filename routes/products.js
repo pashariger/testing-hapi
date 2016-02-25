b382d4e6c5beebb12109f265ba0f5d170599772c
Pasha Riger <pasharx@gmail.com>
2016-02-24 19:41:41 -0500

    conform to hapi eslint style

--- routes/products.js
+++ routes/products.js
@@ -1,78 +1,78 @@
-'use strict'
+'use strict';
 
-const Joi           = require('joi')
-const Handlers      = require('../handlers/products')
-const SCHEMAS       = require('../lib/schemas')
+const Joi           = require('joi');
+const Handlers      = require('../handlers/products');
+const SCHEMAS       = require('../lib/schemas');
 
-const API_BASE_PATH = '/api/products'
+const API_BASE_PATH = '/api/products';
 
-const routes = []
+const routes = [];
 
 // GET /api/products
 routes.push({
-  method: 'GET',
-  path: API_BASE_PATH,
-  config: {
-    auth: false,
-    handler: Handlers.handlers.get,
-    description: 'get products',
-    notes: 'This endpoint allows for the retrieval of products.',
-    plugins: {
-      'hapi-swagger': {
-        responses: {
-          '200': { description: 'Success', schema: Joi.object({
-            result: Joi.array().items(SCHEMAS.Product)
-          }).label('Response') },
-          '400': { description: 'Bad Request', schema: SCHEMAS.Error }
+    method: 'GET',
+    path: API_BASE_PATH,
+    config: {
+        auth: false,
+        handler: Handlers.handlers.get,
+        description: 'get products',
+        notes: 'This endpoint allows for the retrieval of products.',
+        plugins: {
+            'hapi-swagger': {
+                responses: {
+                    '200': { description: 'Success', schema: Joi.object({
+                        result: Joi.array().items(SCHEMAS.Product)
+                    }).label('Response') },
+                    '400': { description: 'Bad Request', schema: SCHEMAS.Error }
+                },
+                security: {}
+            }
         },
-        security: {}
-      }
-    },
-    tags: ['api'],
-    validate: {
-      query: {
-      }
-    },
-    response: {
-      schema: Joi.object({
-        result: Joi.array().items(SCHEMAS.Product)
-      }).label('Response')
+        tags: ['api'],
+        validate: {
+            query: {
+            }
+        },
+        response: {
+            schema: Joi.object({
+                result: Joi.array().items(SCHEMAS.Product)
+            }).label('Response')
+        }
     }
-  }
-})
+});
 
 // GET /api/products/{id}
 routes.push({
-  method: 'GET',
-  path: API_BASE_PATH + '/{id}',
-  config: {
-    auth: false,
-    handler: Handlers.handlers.get,
-    description: 'get product by id',
-    notes: 'This endpoint allows for the retrieval of products.',
-    plugins: {
-      'hapi-swagger': {
-        responses: {
-          '200': { description: 'Success', schema: Joi.object({
-            result: SCHEMAS.Product
-          }).label('Response') },
-          '400': { description: 'Bad Request', schema: SCHEMAS.Error }
+    method: 'GET',
+    path: API_BASE_PATH + '/{id}',
+    config: {
+        auth: false,
+        handler: Handlers.handlers.get,
+        description: 'get product by id',
+        notes: 'This endpoint allows for the retrieval of products.',
+        plugins: {
+            'hapi-swagger': {
+                responses: {
+                    '200': { description: 'Success', schema: Joi.object({
+                        result: SCHEMAS.Product
+                    }).label('Response') },
+                    '400': { description: 'Bad Request', schema: SCHEMAS.Error }
+                },
+                security: {}
+            }
         },
-        security: {}
-      }
-    },
-    tags: ['api'],
-    validate: {
-      params: {
-        id: Joi.number().required()
-      }
-    },
-    response: {
-      schema: Joi.object({
-        result: SCHEMAS.Product
-      }).label('Response')
+        tags: ['api'],
+        validate: {
+            params: {
+                id: Joi.number().required()
+            }
+        },
+        response: {
+            schema: Joi.object({
+                result: SCHEMAS.Product
+            }).label('Response')
+        }
     }
-  }
-})
+});
 
-module.exports = routes
+module.exports = routes;
--- test/unit/products.js
+++ test/unit/products.js
@@ -1,37 +1,44 @@
-'use strict'
+'use strict';
 
 // requires for testing
-const Code      = require('code')
-const expect    = Code.expect
-const Lab       = require('lab')
-const lab       = exports.lab = Lab.script()
+const Code      = require('code');
+const expect    = Code.expect;
+const Lab       = require('lab');
+const lab       = exports.lab = Lab.script();
 
 // use some BDD verbage instead of lab default
-const describe  = lab.describe
-const it        = lab.it
+const describe  = lab.describe;
+const it        = lab.it;
 
 // we require the handlers directly, so we can test the "Lib" functions in isolation
-const ProductHandlers = require('../../handlers/products')
+const ProductHandlers = require('../../handlers/products');
 
 describe('unit tests - products', () => {
-  it('should return all products', (done) => {
-    // test lib function
-    ProductHandlers.lib.getProducts().done((products) => {
-      expect(products).to.be.an.array().and.have.length(2)
 
-      done()
-    }, (err) => {
-      done(err)
-    })
-  })
+    it('should return all products', (done) => {
 
-  it('should return single product', (done) => {
-    ProductHandlers.lib.getProducts(1).done((product) => {
-      expect(product).to.be.an.object()
+        // test lib function
+        ProductHandlers.lib.getProducts().done((products) => {
 
-      done()
-    }, (err) => {
-      done(err)
-    })
-  })
-})
+            expect(products).to.be.an.array().and.have.length(2);
+
+            done();
+        }, (err) => {
+
+            done(err);
+        });
+    });
+
+    it('should return single product', (done) => {
+
+        ProductHandlers.lib.getProducts(1).done((product) => {
+
+            expect(product).to.be.an.object();
+
+            done();
+        }, (err) => {
+
+            done(err);
+        });
+    });
+});
